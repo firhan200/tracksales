e8ff276f5416098b4271ca916e33a55748c1eac5
Firhan <firhan.faisal1995@gmail.com>
2023-07-26 06:33:49 +0700

    feat(docker): add production config

--- next.config.js
+++ next.config.js
@@ -8,4 +8,11 @@ module.exports = {
   //   }
   //   return config
   // },
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
 }
